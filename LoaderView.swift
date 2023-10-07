c01f360efcc2c993ac53b9e3a9ce762085516c6b
Jayant Badlani <badlanijayant9@gmail.com>
2023-10-07 13:08:06 +0530

    LoaderView and Readme added.

---
+++ LoaderView.swift
@@ -0,0 +1,103 @@
+//
+//  LoaderView.swift
+//  Pro Caller
+//
+//  Created by Jayant on 07/10/23.
+//
+
+
+import SwiftUI
+
+struct LoaderView: View {
+    
+    var text: String?
+    var userInteractionEnable: Bool = false
+    var backgroundColor: Color = .white
+    var tintColor: Color = .black
+    
+    @State private var isAnimating: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if userInteractionEnable {
+                Color.clear
+            } else {
+                Color.black.opacity(0.00001)
+                    .onTapGesture { /* Do nothing*/ }
+            }
+            
+            VStack(spacing: 20) {
+                Circle()
+                    .trim(from: 0, to: 1)
+                    .stroke(
+                        AngularGradient(gradient: Gradient(colors: [tintColor.opacity(0), tintColor.opacity(0.8)]),
+                                        center: .center,
+                                        startAngle: .degrees(0),
+                                        endAngle: .degrees(360)),
+                        style: StrokeStyle(lineWidth: 2, lineCap: .round)
+                    )
+                    .frame(width: 47, height: 47)
+                    .rotationEffect(Angle(degrees: isAnimating ? 360 : 0))
+                
+                if let text = text, !text.isEmpty {
+                    Text(text)
+                        .multilineTextAlignment(.center)
+                        .font(.system(size: 16, weight: .semibold))
+                        .foregroundColor(tintColor)
+                        .frame(maxWidth: UIScreen.main.bounds.width - 150)
+                }
+            }
+            .padding(.vertical, 20)
+            .padding(.horizontal, 20)
+            .background {
+                Rectangle()
+                    .cornerRadius(15)
+                    .foregroundColor(backgroundColor)
+            }
+        }
+        .onAppear {
+            withAnimation(Animation.linear(duration: 1.0).repeatForever(autoreverses: false)) {
+                isAnimating = true
+            }
+        }
+        .onDisappear {
+            isAnimating = false
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct LoaderViewModifier: ViewModifier {
+    
+    @Binding var isShow: Bool
+    var text: String?
+    var userInteractionEnable: Bool = false
+    var backgroundColor: Color = .white
+    var tintColor: Color = .black
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            content
+            if isShow {
+                LoaderView(text: text, userInteractionEnable: userInteractionEnable, backgroundColor: backgroundColor, tintColor: tintColor)
+                    .ignoresSafeArea()
+            }
+        }
+    }
+}
+
+extension View {
+    func loader(isShow: Binding<Bool>, userInteractionEnable: Bool = false, backgroundColor: Color = .white, text: String? = nil) -> some View {
+        self.modifier(LoaderViewModifier(isShow: isShow, text: text, userInteractionEnable: userInteractionEnable, backgroundColor: backgroundColor))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        Rectangle()
+            .foregroundColor(.accentColor)
+            .edgesIgnoringSafeArea(.all)
+            .loader(isShow: .constant(true), text: "Loading...")
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,113 @@
 # SVProgressHUD-SwiftUI
 SVProgressHUD_SwiftUI is a clean and easy-to-use HUD meant to display the progress of an ongoing task in SwiftUI apps
+
+
+### Description
+LoaderView is a SwiftUI component that provides a customizable loading indicator for your iOS app. It's designed to give you full control over the loader's appearance and behavior, making it easy to integrate and use in your SwiftUI projects. Similar to popular libraries like SVProgressHUD, LoaderView is entirely written in SwiftUI.
+
+## Installation
+To use LoaderView in your SwiftUI project, copy the `LoaderView.swift` file into your project directory and use this modifier.
+
+```swift
+.loader(isShow: $isLoading)
+```
+
+## Usage
+You can use LoaderView in your SwiftUI project in two different ways:
+
+### Option 1: Using the `.loader` Modifier
+
+```swift
+import SwiftUI
+
+struct ContentView: View {
+@State private var isLoading = false
+
+var body: some View {
+VStack {
+Text("Hello, LoaderView!")
+.padding()
+
+Button("Show Loader") {
+isLoading.toggle()
+}
+.loader(isShow: $isLoading, text: "Loading...")
+}
+}
+}
+
+struct ContentView_Previews: PreviewProvider {
+static var previews: some View {
+ContentView()
+}
+}
+```
+
+### Option 2: Integrating LoaderView within a ZStack
+
+```ruby
+import SwiftUI
+
+struct ContentView: View {
+@State private var otp: String = ""
+@FocusState private var isOTPFieldFocused: Bool
+private let numberOfFieldsInOTP = 6
+
+var body: some View {
+
+VStack(alignment: .leading, spacing: 8) {
+Text("VERIFICATION CODE")
+.foregroundColor(Color.gray)
+.font(.system(size: 12))
+
+OTPFieldView(numberOfFields: numberOfFieldsInOTP, otp: $otp)
+.onChange(of: otp) { newOtp in
+if newOtp.count == numberOfFieldsInOTP {
+// Verify OTP
+}
+}
+.focused($isTextFieldFocused)
+
+Text("Entered OTP: \(otp)")
+}
+.onAppear {
+DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+isTextFieldFocused = true
+}
+}
+}
+}
+
+struct ContentView_Previews: PreviewProvider {
+static var previews: some View {
+ContentView()
+}
+}
+```
+
+
+## Customize LoaderView properties as needed:
+
+###text: An optional string to display a loading message.
+
+###userInteractionEnable: A boolean to enable or disable user interaction while the loader is visible.
+
+###backgroundColor: The background color of the loader (default is .white).
+
+###tintColor: The color of the loader's animated circle (default is .black).
+
+Feel free to customize and enhance LoaderView according to your specific use case, and enjoy its user-friendly features in your SwiftUI app!
+
+## Requirements
+iOS 
+Xcode 
+Swift
+
+##  Contributions
+We welcome contributions and suggestions from the community. If you encounter issues or have ideas for improvements, please feel free to open an issue or create a pull request.
+
+## Author
+Jayant Badlani
+
+## Acknowledgments
+This LoaderView component was developed to provide a customizable loading indicator for SwiftUI applications, enhancing user experience during asynchronous tasks.
